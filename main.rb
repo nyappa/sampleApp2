ad4ef8fcb188a5dfaf11a7cab11de31cf2008a68
asuke <asuke@www3227gj.sakura.ne.jp>
2015-02-03 09:22:55 +0900

    Add unicorn and run files

--- main.rb
+++ main.rb
@@ -1,13 +1,9 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'active_record'
 require 'yaml'
 require "json"
 require "json-fuzz-generator"
 require 'logger'
-
-# start setting for server run 
-set :environment, :production
-enable :method_override
 
 # connecting databases
 config = YAML.load_file('database.yml')
@@ -22,85 +18,91 @@ end
 class Word < ActiveRecord::Base
 end
 
-helpers do
-    #自前で書かないとpartial出来んらしい
-    def partial(template, options={})
-        erb "_#{template}".to_sym, options.merge(:layout => false)
+class MainApp < Sinatra::Base
+    # start setting for server run 
+    set :environment, :production
+    enable :method_override
+
+    helpers do
+        #自前で書かないとpartial出来んらしい
+        def partial(template, options={})
+            erb "_#{template}".to_sym, options.merge(:layout => false)
+        end
     end
-end
 
-# main codes
-get '/' do
-  erb :index
-end
+    get '/' do
+        erb :index
+    end
 
-#word codes
-post'/word.json' do
-    content_type :json
-    word         = Word.new
-    word.word    = @params[:word]
-    word.meaning = @params[:meaning]
-    word.text_id = @params[:text_id]
-    word.status  = @params[:status]
-    word.save
-    { :status => "sucsess" }.to_json
-end
+    #word codes
+    post'/word.json' do
+        content_type :json
+        word         = Word.new
+        word.word    = @params[:word]
+        word.meaning = @params[:meaning]
+        word.text_id = @params[:text_id]
+        word.status  = @params[:status]
+        word.save
+        { :status => "sucsess" }.to_json
+    end
 
-patch'/word.json' do
-    content_type :json
-    word = Word.find(@params[:id])
-    word.status = @params[:status]
-    word.save
-    word.to_json
-end
+    patch'/word.json' do
+        content_type :json
+        word = Word.find(@params[:id])
+        word.status = @params[:status]
+        word.save
+        word.to_json
+    end
 
-get'/word.json' do
-    content_type :json
-    Word.find(@params[:id]).to_json
-end
+    get'/word.json' do
+        content_type :json
+        Word.find(@params[:id]).to_json
+    end
 
-delete '/word.json' do
-    content_type :json
-    Word.find(@params[:id]).delete
-    { :status => "sucsess" }.to_json
-end
+    delete '/word.json' do
+        content_type :json
+        Word.find(@params[:id]).delete
+        { :status => "sucsess" }.to_json
+    end
 
-get'/words.json' do
-    content_type :json
-    Word.where(["text_id = ?", @params[:id]]).to_json
-end
+    get'/words.json' do
+        content_type :json
+        Word.where(["text_id = ?", @params[:id]]).to_json
+    end
 
-# text codes
-post'/generate_data.json' do
-    content_type :json
-    eng        = ReadEngTexts.new
-    eng.title  = @params[:title]
-    eng.text   = @params[:text]
-    eng.status = @params[:status]
-    eng.save
-    ReadEngTexts.all.to_json
-end
+    # text codes
+    post'/generate_data.json' do
+        content_type :json
+        eng        = ReadEngTexts.new
+        eng.title  = @params[:title]
+        eng.text   = @params[:text]
+        eng.status = @params[:status]
+        eng.save
+        ReadEngTexts.all.to_json
+    end
 
-patch'/text_detail.json' do
-    content_type :json
-    eng = ReadEngTexts.find(@params[:id])
-    eng.status = @params[:status]
-    eng.save
-    { :status => "sucsess" }.to_json
-end
+    patch'/text_detail.json' do
+        content_type :json
+        eng = ReadEngTexts.find(@params[:id])
+        eng.status = @params[:status]
+        eng.save
+        { :status => "sucsess" }.to_json
+    end
 
-get'/generate_data.json' do
-    content_type :json
-    ReadEngTexts.all.to_json
-end
+    get'/generate_data.json' do
+        content_type :json
+        ReadEngTexts.all.to_json
+    end
 
-get '/text_detail.json' do
-    content_type :json
-    ReadEngTexts.find(@params[:id]).to_json
-end
+    get '/text_detail.json' do
+        content_type :json
+        ReadEngTexts.find(@params[:id]).to_json
+    end
 
-delete '/text_detail.json' do
-    content_type :json
-    ReadEngTexts.find(@params[:id]).delete
-    { :status => "sucsess" }.to_json
+    delete '/text_detail.json' do
+        content_type :json
+        ReadEngTexts.find(@params[:id]).delete
+        { :status => "sucsess" }.to_json
+    end
+
 end

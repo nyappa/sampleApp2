980ab0e6b4cdfcfc1c119b901f8c437d4fa7b738
asuke <asuke@www3227gj.sakura.ne.jp>
2015-02-05 15:59:24 +0900

    delete unnecessary files

--- public/js/contents/apps/model.js
+++
@@ -1,7 +0,0 @@
-Apps.module('Model', function (Model, App, Backbone) {
-    Model.Apps            = Backbone.Model.extend({});
-    Model.AppsCollection  = Backbone.Collection.extend({
-        model: Model.Apps,
-        url   : '/apps.json'
-    });
-});
--- public/js/contents/common2.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    var count = 0;
-    window.setInterval(function(){ 
-        count++
-            $(".js-seisan-count").text(count);
-    }, 1000);
-});
